ad709b1ec1f1dfffec190c9daef5e12296d418fc
Hairo <hairocr8@gmail.com>
2023-02-20 22:57:09 -0400

    First upload

---
+++ FUCTool.py
@@ -0,0 +1,473 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import shutil
+import sys
+from pathlib import Path
+
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+
+import utils
+from qt_ui import Ui_MainWindow
+
+
+class QTextEditLogger(logging.Handler, QtCore.QObject):
+    appendPlainText = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent):
+        super().__init__()
+        QtCore.QObject.__init__(self)
+        self.widget = QtWidgets.QPlainTextEdit(parent)
+        self.widget.setReadOnly(True)
+        self.appendPlainText.connect(self.widget.appendPlainText)
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.appendPlainText.emit(msg)
+
+
+class OptionalWidget(QtWidgets.QWidget):
+    def __init__(self, label, filename, parent=None):
+        super(OptionalWidget, self).__init__(parent)
+        self.filename = filename
+
+        label = QtWidgets.QLabel(label)
+        self.checkbox = QtWidgets.QCheckBox()
+
+        # sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        # self.checkbox.setSizePolicy(sizePolicy)
+        label.setWordWrap(True)
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(self.checkbox)
+
+        self.mouseReleaseEvent = lambda _: self.checkbox.click()
+        self.setLayout(layout)
+
+
+class ConfigWidget(QtWidgets.QWidget):
+    def __init__(self, desc, options, parent=None):
+        super(ConfigWidget, self).__init__(parent)
+        self.options = options
+
+        label = QtWidgets.QLabel(desc)
+        self.combobox = QtWidgets.QComboBox()
+
+        for op in self.options["values"]:
+            self.combobox.addItem(op["label"])
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        self.combobox.setSizePolicy(sizePolicy)
+        label.setWordWrap(True)
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(self.combobox)
+
+        self.setLayout(layout)
+
+
+class DumpDataBINThread(QtCore.QThread):
+    endSignal = QtCore.pyqtSignal(str)
+    statusSignal = QtCore.pyqtSignal(int)
+
+    def __init__(self, filepath):
+        super().__init__()
+        self.filepath = filepath
+
+    def run(self):
+        # bin_dec = Path(self.filepath).stem + ".BIN.DEC"
+        # utils.decrypt_data_bin(self.filepath, bin_dec)
+        # self.statusSignal.emit(1)
+
+        outfolder = Path(self.filepath).parent.joinpath("DATA.BIN_dump")
+
+        # Check if folder exists already, can cause issues later
+        if outfolder.exists():
+            shutil.rmtree(outfolder)
+
+        os.makedirs(outfolder, exist_ok=True)
+
+        try:
+            utils.dump_data_bin(self.filepath, outfolder)
+            self.statusSignal.emit(1)
+        except (MemoryError, OverflowError):
+            self.statusSignal.emit(-1)
+            return
+
+        utils.rename_dump_files(outfolder)
+        self.endSignal.emit(str(outfolder.absolute()))
+
+
+class QuestsReadThread(QtCore.QThread):
+    endSignal = QtCore.pyqtSignal(str)
+
+    def __init__(self, folderpath):
+        super().__init__()
+        self.folderpath = folderpath
+
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.dump_thread = None
+
+        self.process1 = None  # UMD-Replace.exe
+        self.process2 = None  # xdelta3.exe
+        self.process3 = None  # psp-save.exe
+
+        self.folder_quests = []
+        self.save_quests = []
+
+        self.save = None
+        self.save_key = None
+        # self.decpath = None
+
+        config = utils.config
+
+        # Cleanup
+        if utils.temp_folder.exists():
+            shutil.rmtree(utils.temp_folder)
+
+        # Patcher Tab
+        logTextBox = QTextEditLogger(self)
+        logTextBox.setFormatter(logging.Formatter('%(levelname)s | %(message)s'))
+        logging.getLogger().addHandler(logTextBox)
+        logging.getLogger().setLevel(logging.INFO)
+        self.patcher_verticalLayout.insertWidget(4, logTextBox.widget)
+
+        self.optional_patches = []
+        for itm in config["optional"]:
+            item = QtWidgets.QListWidgetItem(self.optional_list)
+            item_widget = OptionalWidget(itm["label"], itm["file"])
+            item.setSizeHint(item_widget.sizeHint())
+            self.optional_list.addItem(item)
+            self.optional_list.setItemWidget(item, item_widget)
+            self.optional_patches.append(item_widget)
+
+        self.patch_button.clicked.connect(self.patch_iso)
+        self.iso_button.clicked.connect(self.select_iso)
+
+        # Config Tab
+        self.config_options = []
+        for itm in config["config.bin"]:
+            item = QtWidgets.QListWidgetItem(self.config_list)
+            item_widget = ConfigWidget(itm["description"], itm["options"])
+            item.setSizeHint(item_widget.sizeHint())
+            self.config_list.addItem(item)
+            self.config_list.setItemWidget(item, item_widget)
+            self.config_options.append(item_widget)
+
+        self.config_button.clicked.connect(self.save_config)
+        self.config_bin_button.clicked.connect(self.select_config_bin)
+
+        # Replacer Tab
+        header = self.replace_list.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+
+        self.replace_folder_button.clicked.connect(self.select_replace_folder)
+        self.refresh_replace_button.clicked.connect(self.refresh_list_clicked)
+        self.nativepsp_button.clicked.connect(self.generate_nativepsp_folder)
+        self.dump_databin_button.clicked.connect(self.dump_databin)
+
+        # Quests Tab
+        self.quests_folder_table.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        self.quests_folder_table.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        self.quests_save_table.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        self.quests_save_table.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+
+        self.scan_quests_folder()
+
+        self.save_folder_button.clicked.connect(self.select_save_folder)
+        self.quests_rescan.clicked.connect(self.scan_quests_folder)
+        self.quests_right.clicked.connect(self.copy_to_save)
+        self.quests_left.clicked.connect(self.copy_from_save)
+        self.quests_remove.clicked.connect(self.remove_from_save)
+
+    def generic_dialog(self, text, title="Info", mode=0):
+        if mode == 0:
+            QtWidgets.QMessageBox.information(self, title, text)
+        if mode == 1:
+            QtWidgets.QMessageBox.critical(self, title, text)
+
+    def select_iso(self):
+        options = QtWidgets.QFileDialog.Options()
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select MHFU ISO file", "", "ISO Files (*.iso)",
+                                                            options=options)
+        if fileName:
+            self.iso_path.setText(fileName)
+            self.optional_list.setEnabled(True)
+            self.patch_button.setEnabled(True)
+
+    def patch_iso(self):
+        print(self.iso_path.text())
+
+        for itm in self.optional_patches:
+            print(itm.filename, itm.checkbox.isChecked())
+
+        # patcher.LOGGER = logging.getLogger()
+        # patcher.replace_data_bin("", "")
+
+    def select_config_bin(self):
+        options = QtWidgets.QFileDialog.Options()
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select config.bin file", "",
+                                                            "config.bin (config.bin)", options=options)
+        if fileName:
+            for i, conf in enumerate(utils.read_configs(fileName)):
+                self.config_options[i].combobox.setCurrentIndex(conf)
+
+            self.config_bin_path.setText(fileName)
+            self.config_list.setEnabled(True)
+            self.config_button.setEnabled(True)
+
+    def save_config(self):
+        cpath = self.config_bin_path.text()
+        config_bin = utils.read_file_bytes(cpath)
+
+        for itm in self.config_options:
+            offset = itm.options["offset"]
+            data = itm.options["values"][itm.combobox.currentIndex()]["data"]
+            config_bin = utils.write_config(config_bin, offset, data)
+
+        utils.write_file_bytes(cpath, config_bin)
+        self.generic_dialog("Configuration saved successfully.")
+
+    def refresh_replace_list(self, folderName):
+        files = utils.read_replace_folder(folderName)
+
+        if not files:
+            self.generic_dialog("ERROR: No data folder found.", mode=1, title="Error")
+            return
+
+        lenght = len(files)
+        self.replace_list.setRowCount(lenght)
+        self.replace_status.setText(f"{lenght} file(s) found.")
+
+        for i, f in enumerate(files):
+            idx = QtWidgets.QTableWidgetItem(f["id"])
+            path = QtWidgets.QTableWidgetItem(f["path"])
+            self.replace_list.setItem(i, 0, idx)
+            self.replace_list.setItem(i, 1, path)
+
+        self.replace_list.resizeRowsToContents()
+
+    def refresh_list_clicked(self):
+        path = self.replace_path.text()
+        self.refresh_replace_list(path)
+
+    def select_replace_folder(self):
+        options = QtWidgets.QFileDialog.Options(QtWidgets.QFileDialog.ShowDirsOnly)
+        folderName = QtWidgets.QFileDialog.getExistingDirectory(self, "Select mods folder", options=options)
+        if folderName:
+            self.refresh_replace_list(folderName)
+
+            self.replace_path.setText(folderName)
+            self.replace_list.setEnabled(True)
+            self.refresh_replace_button.setEnabled(True)
+            self.nativepsp_button.setEnabled(True)
+
+    def generate_nativepsp_folder(self):
+        inpath = self.replace_path.text()
+        outpath = Path(inpath).parent.absolute().joinpath('nativePSP')
+        utils.generate_filebin(inpath, outpath)
+
+        self.generic_dialog(f"nativePSP folder successfully generated at: {outpath}")
+
+    def dump_databin(self):
+        options = QtWidgets.QFileDialog.Options()
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select DATA.BIN file", "",
+                                                            "DATA.BIN (DATA.BIN DATA.BIN.DEC)",
+                                                            options=options)
+        if fileName:
+            self.dump_databin_button.setEnabled(False)
+            self.dump_databin_button.setText("Dumping...")
+
+            self.dump_thread = DumpDataBINThread(fileName)
+            self.dump_thread.start()
+
+            self.dump_thread.endSignal.connect(self.dump_end)
+            self.dump_thread.statusSignal.connect(self.dump_status)
+
+    def dump_status(self, code):
+        if code == -1:
+            self.dump_databin_button.setText("Dump DATA.BIN")
+            self.dump_databin_button.setEnabled(True)
+            self.generic_dialog(f"DATA.BIN is not decrypted.", mode=1, title="Error")
+            self.dump_thread.exit()
+        if code == 1:
+            self.dump_databin_button.setText("Renaming...")
+
+    def dump_end(self, filepath):
+        self.dump_databin_button.setEnabled(True)
+        self.dump_databin_button.setText("Dump DATA.BIN")
+
+        self.generic_dialog(f"DATA.BIN dumped to {filepath}")
+        self.dump_thread.exit()
+
+    def scan_quests_folder(self):
+        self.quests_save_table.clearSelection()
+        self.folder_quests = utils.get_quests_in_folder()
+        qsize = len(self.folder_quests)
+
+        self.quests_folder_table.setRowCount(qsize)
+        self.folder_count.setText(f"Quests in folder ({qsize}):")
+
+        for i, q in enumerate(self.folder_quests):
+            qid = QtWidgets.QTableWidgetItem(q["qid"])
+            name = QtWidgets.QTableWidgetItem(q["name"])
+            self.quests_folder_table.setItem(i, 0, qid)
+            self.quests_folder_table.setItem(i, 1, name)
+
+    def scan_quests_save(self):
+        qsize = len(self.save_quests)
+        self.save_count.setText(f"Quests in save ({qsize}/18):")
+
+        for i, q in enumerate(self.save_quests):
+            qid = QtWidgets.QTableWidgetItem(q["qid"])
+            name = QtWidgets.QTableWidgetItem(q["name"])
+            self.quests_save_table.setItem(i, 0, qid)
+            self.quests_save_table.setItem(i, 1, name)
+
+    def select_save_folder(self):
+        options = QtWidgets.QFileDialog.Options(QtWidgets.QFileDialog.ShowDirsOnly)
+        folderName = QtWidgets.QFileDialog.getExistingDirectory(self, "Select PSP save folder", options=options)
+        if folderName:
+            savepath = Path(folderName).joinpath("MHP2NDG.BIN")
+            self.save_path.setText(folderName)
+
+            # TODO: move to QuestsReadThread
+            self.read_save(savepath)
+
+            self.quests_right.setEnabled(True)
+            self.quests_left.setEnabled(True)
+            self.quests_remove.setEnabled(True)
+            self.quests_save_table.setEnabled(True)
+            self.quests_save_button.setEnabled(True)
+
+    def read_save(self, path):
+        mode = None
+        if "ULES01213" in path.parent.name:
+            self.save_key = "FU.bin"
+            mode = 1
+        if  "ULUS10391" in path.parent.name:
+            self.save_key = "FU.bin"
+            mode = 2
+        if "ULJM05500" in path.parent.name:
+            self.save_key = "P2G.bin"
+            mode = 3
+
+        param = Path(path.parent, "PARAM.SFO")
+        if (self.save_key is None) or (not path.exists()) or (not param.exists()):
+            self.generic_dialog("Select the PSP save folder with the MHP2NDG.BIN and PARAM.SFO files.",
+                                mode=1, title="Error")
+            return
+
+        shutil.copy2(path, Path(path.parent, path.stem + ".BIN.BAK"))  # Backup save just in case
+
+        dec = utils.decrypt_save(path, mode)
+        self.save = dec
+        self.save_quests = utils.get_quests_in_save(dec)
+        self.scan_quests_save()
+
+    # def decrypt_finished(self):
+    #     self.process3 = None
+    #
+    #     self.save_folder_button.setText("Select")
+    #     self.save = utils.read_file_bytes(self.decpath)
+    #
+    #     self.quests_right.setEnabled(True)
+    #     self.quests_left.setEnabled(True)
+    #     self.quests_save_table.setEnabled(True)
+
+    def copy_to_save(self):
+        selection = self.quests_folder_table.selectionModel().selectedRows()
+
+        # TODO: Check if it's an arena quest and not add it (and show a dialog)
+        if len(self.save_quests)+len(selection) > 18:
+            self.generic_dialog("Not enough slots to add selected quests to save.", mode=1, title="Error")
+        else:
+            for s in selection:
+                # TODO: encrypt when writting save
+                # enc = utils.encrypt_quest(qfile)
+
+                qfile = self.folder_quests[s.row()]
+                self.save_quests.append(qfile)
+
+            self.quests_folder_table.clearSelection()
+            self.scan_quests_save()
+
+    def copy_from_save(self):
+        selection = self.quests_save_table.selectionModel().selectedRows()
+
+        for s in selection:
+            qfile = self.save_quests[s.row()]
+            self.folder_quests.append(qfile)
+
+            fname = "m"+qfile["qid"]+".mib.dec"
+            fpath = Path(utils.current_path, "quests", fname)
+
+            if fpath.exists():
+                dlg = QtWidgets.QMessageBox()
+                dlg.setWindowTitle("Question")
+                dlg.setText(f"{fname} already exists, overwrite?")
+                dlg.setIcon(QtWidgets.QMessageBox.Icon.Question)
+                dlg.setStandardButtons(
+                    QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No)
+
+                if dlg.exec() == QtWidgets.QMessageBox.StandardButton.Yes:
+                    with open(fpath, "wb") as f:
+                        f.write(qfile["bytes"])
+            else:
+                with open(fpath, "wb") as f:
+                    f.write(qfile["bytes"])
+
+        self.scan_quests_folder()
+
+    def remove_from_save(self):
+        selection = self.quests_save_table.selectionModel().selectedRows()
+
+        removed = []
+        for s in selection:
+            removed.append(s.row())
+
+        nlist = []
+        for i in range(len(self.save_quests)):
+            if i not in removed:
+                nlist.append(self.save_quests[i])
+
+        self.save_quests = nlist
+        self.quests_save_table.clearContents()
+        self.scan_quests_save()
+
+    def write_save(self, path):
+        # keypath = Path("res", self.save_key)
+        # utils.create_temp_folder()
+        # decpath = Path(utils.temp_folder, path.stem + ".BIN.DEC")
+        # exe_path = Path(utils.current_path, "bin", "psp-save-w32.exe")
+
+        self.save_folder_button.setText("Decrypting...")
+        self.process3 = QtCore.QProcess()
+        self.process3.finished.connect(self.decrypt_finished)
+        self.process3.start(str(exe_path), ["-d", str(keypath), "5", str(path), str(decpath)])
+
+    def encrypt_and_save(self):
+        keypath = Path("res", self.save_key)
+        exe_path = Path(utils.current_path, "bin", "psp-save-w32.exe")
+
+        self.quests_save_button.setText("Encrypting...")
+
+        # self.decpath
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    app.setStyle("Fusion")
+    window.show()
+    app.exec_()
---
+++ utils.py
@@ -0,0 +1,308 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import csv
+import glob
+import json
+import os
+import shutil
+import sys
+from pathlib import Path, PureWindowsPath
+
+import bitstring
+
+import mhef.psp
+import mhff.psp.data
+
+UMD_MD5HASH = "1f76ee9ccbd6d39158f06e6e5354a5bd"
+PSN_MD5HASH = "cc39d070b2d2c44c9ac8187e00b75dc4"
+PATCHED_MD5HASH = "1890386601adf7d1d3d69067e8917c83"  # TODO: Cambiar con la version final
+
+QUESTS_START = 0x142300
+QUESTS_END = 0x169360
+QUESTS_SIZE = 0x22B0
+
+
+def read_file_bytes(filepath):
+    with open(filepath, "rb") as f:
+        bfile = f.read()
+
+    return bytearray(bfile)
+
+
+def write_file_bytes(filepath, barray):
+    with open(filepath, "wb") as f:
+        f.write(barray)
+
+
+def int_to_bytes(x):
+    if x == 0:
+        return b'\x00'
+    else:
+        return x.to_bytes((x.bit_length() + 7) // 8, 'little')
+
+
+def create_temp_folder():
+    if not temp_folder.exists():
+        os.makedirs(temp_folder)
+
+
+def write_config(barray, offset, data):
+    offset = int(offset, 16)
+    data = bytes.fromhex(data[2:])
+    dsize = len(data)
+
+    # Expand file if the offset is bigger
+    if len(barray) < offset + len(data):
+        ext = (offset + len(data)) - len(barray)
+        barray.extend(b'\x00' * ext)
+
+    for i in range(dsize):
+        barray[offset + i] = data[i]
+
+    return barray
+
+
+def read_configs(config_path):
+    configbin = read_file_bytes(config_path)
+    values = []
+
+    for itm in config["config.bin"]:
+        offset = int(itm["options"]["offset"], 16)
+        jvalues = [bytes.fromhex(i["data"][2:]) for i in itm["options"]["values"]]
+
+        file_value = int_to_bytes(configbin[offset])
+
+        if file_value in jvalues:
+            index = None
+            for i, v in enumerate(itm["options"]["values"]):
+                if bytes.fromhex(v["data"][2:]) == file_value:
+                    index = i
+                    break
+
+            values.append(index)
+        else:
+            values.append(0)  # current value is not in the json options
+
+    return values
+
+
+def get_all_files(folderpath):
+    return glob.glob(f"{folderpath}/**/*.*", recursive=True)
+
+
+def read_replace_folder(infolder):
+    infiles = get_all_files(infolder)
+
+    existing_files = []
+
+    # check if the data folder exists
+    if not Path(infolder, "data").exists():
+        return existing_files
+
+    for f in infiles:
+        key = PureWindowsPath(f).relative_to(infolder).as_posix()
+        try:
+            idx = filelist[key]
+            existing_files.append({"path": key, "id": idx})
+        except KeyError:
+            continue
+
+    existing_files.sort(key=lambda k: k['id'])
+
+    return existing_files
+
+
+def get_filebin_data(filename):
+    filebin = read_file_bytes(filename)
+
+    files = []
+    for i, byte in enumerate(filebin):
+        if byte != 0x00:
+            barray = bitstring.BitArray(uint=byte, length=8)
+
+            for j, bit in enumerate(barray[::-1]):  # type: ignore
+                if bit:
+                    files.append(i * 8 + j)
+
+    return files
+
+
+def generate_filebin(infolder, outfolder):
+    infiles = get_all_files(infolder)
+
+    file_ids = []
+    existing_files = []
+    for fi in infiles:
+        key = fi.replace(f"{infolder}/", "")
+        idx = filelist[key]
+        file_ids.append(idx)
+        existing_files.append({"path": fi, "id": idx})
+
+    newbin = bitstring.BitArray(uint=0, length=826 * 8)
+
+    for f in file_ids:
+        idx = int(f)
+        block = int(idx / 8)
+        offset = idx % 8
+        index = (block * 8) + (7 - offset)
+        newbin[index] = True
+
+    if not os.path.isdir(outfolder):
+        os.mkdir(outfolder)
+
+    # copy/rename files and save file.bin
+    with open(Path(outfolder, "file.bin"), "wb") as f:
+        newbin.tofile(f)
+
+    copy_files(existing_files, outfolder)
+
+
+def copy_files(allfiles, outfolder):
+    for f in allfiles:
+        shutil.copy2(Path(f['path']), Path(outfolder, f['id']))
+
+
+def rename_dump_files(outfolder):
+    allfiles = glob.glob(f"{outfolder}/*", recursive=True)
+    inv_filelist = dict((v, k) for k, v in filelist.items())
+
+    for f in allfiles:
+        npath = inv_filelist[Path(f).name]
+        new_folders = Path(outfolder).joinpath(Path(npath).parent)
+        os.makedirs(new_folders, exist_ok=True)
+
+        dest = Path(outfolder).joinpath(npath)
+        shutil.move(f, dest)
+
+
+def decrypt_data_bin(data_bin, outpath):
+    dc = mhef.psp.DataCipher(mhef.psp.MHP2G_JP)
+    dc.decrypt_file(data_bin, outpath)
+
+
+def dump_data_bin(data_bin, outfolder):
+    mhff.psp.data.extract(data_bin, outfolder)
+
+
+def decrypt_save(filepath, mode):
+    game = None
+
+    if mode == 1:
+        game = mhef.psp.MHP2G_EU
+    if mode == 2:
+        game = mhef.psp.MHP2G_NA
+    if mode == 3:
+        game = mhef.psp.MHP2G_JP
+
+    sfile = read_file_bytes(filepath)
+
+    psc = mhef.psp.PSPSavedataCipher(game)
+    sfile = psc.decrypt(sfile)
+
+    sc = mhef.psp.SavedataCipher(game)
+    dec = sc.decrypt(sfile)
+
+    return dec
+
+
+def get_quest_data(qfile):
+    current = 0x80
+    while qfile[current] != 0x00:
+        current += 1
+
+    name = qfile[0x80:current].decode("utf-8")
+    qid = int.from_bytes(qfile[0x64:0x66], byteorder='little')
+
+    return str(qid), name
+
+
+def get_quests_in_folder():
+    quests = glob.glob("quests/*.mib.dec")+glob.glob("quests/*.pat")
+
+    res = []
+    for q in quests:
+        if Path(q).suffix == ".pat":
+            qfile = read_file_bytes(q)[0x04:]
+        else:
+            qfile = read_file_bytes(q)
+
+        qid, name = get_quest_data(qfile)
+        res.append({"bytes": q, "qid": qid, "name": name})
+
+    res = sorted(res, key=lambda d: d['qid'])
+    return res
+
+
+def get_quests_in_save(save_file):
+    res = []
+    for i in range(QUESTS_START, QUESTS_END, QUESTS_SIZE):
+        current = i
+        while save_file[current:current + 0x4] != b'\x00\x00\x00\x00':
+            current += 1
+
+        if current - i > 0:  # Check for empty slots
+            dec = decrypt_quest(save_file[i:current])
+
+            if dec is not None:
+                qid, name = get_quest_data(dec)
+                res.append({"bytes": dec, "qid": qid, "name": name})
+
+    return res
+
+
+def encrypt_quest(quest):
+    qc = mhef.psp.QuestCipher(mhef.psp.MHP2G_JP)
+    enc = qc.encrypt(quest)
+
+    return enc
+
+
+def decrypt_quest(quest):
+    # Very ugly but, yeah...
+    try:
+        qc = mhef.psp.QuestCipher(mhef.psp.MHP2G_JP)
+        dec = qc.decrypt(quest)
+        return dec
+    except ValueError:
+        pass
+
+    try:
+        qc = mhef.psp.QuestCipher(mhef.psp.MHP2G_NA)
+        dec = qc.decrypt(quest)
+        return dec
+    except ValueError:
+        pass
+
+    try:
+        qc = mhef.psp.QuestCipher(mhef.psp.MHP2G_EU)
+        dec = qc.decrypt(quest)
+        return dec
+    except ValueError:
+        pass
+
+    return None
+
+
+def get_config_json(filename):
+    with open(filename) as f:
+        nconfig = json.loads(f.read())
+
+    return nconfig
+
+
+def get_filelist(filename):
+    nfilelist = {}
+    with open(filename) as f:
+        reader = csv.reader(f)
+        for idx, path in reader:
+            nfilelist.setdefault(path, idx)
+
+    return nfilelist
+
+
+config = get_config_json("res/config.json")
+filelist = get_filelist("res/filelist_2g.csv")
+# current_path = Path(sys.executable).parent.resolve()
+current_path = Path(__file__).resolve().parent
+temp_folder = Path(current_path, "res", "temp")
